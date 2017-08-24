649c2ebf4084d00180c4af2bde67cf333718806a
zhangyangyang <admin@LAPTOP-5V8AISB8>
2017-08-24 13:27:48 +0800

    somate-maven项目

---
+++ src/main/java/com/program/somate/config/SomateConfig.java
@@ -0,0 +1,49 @@
+package com.program.somate.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.template.Engine;
+
+public class SomateConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configEngine(Engine arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configHandler(Handlers arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configInterceptor(Interceptors arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configRoute(Routes arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
